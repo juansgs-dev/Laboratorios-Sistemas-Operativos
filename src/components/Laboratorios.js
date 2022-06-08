490f97ef170eeab71897342776463c685bab8554
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2022-06-08 09:21:14 -0500

    V2 bug de modulo interrupcion

--- src/components/Laboratorios.js
+++ src/components/Laboratorios.js
@@ -10,13 +10,13 @@ export const Laboratorios = () => {
                 <div className='Img'>
                     <img src={Interrupcion} alt='imagen Interrupcion'></img>
                 </div>
-                <span>Ejemplo de una interrupción que se ocasiona al querer ingresar un número impar y un correo electrónico invalido.</span>
-                <a href="Interrupcion"><button className='button'>
+                <span>Ejemplo de una interrupción que se ocasiona al querer ingresar un número impar y un nombre.</span>
+                <a href="Interrupcion" className='button'>
                     <span>Ir al Ejemplo</span>
                     <div className='icono'>
                         <i class="fa-solid fa-arrow-right"></i>
                     </div>
-                </button></a>
+                </a>
             </section>
 
         </>
